a33633a5aa5ec92c4a71e98bcf0b7c827bea43d2
Michael Petrou <mpetrou74@hotmail.com>
2025-03-06 14:22:32 +1030

    updated setup script to correctly create the remote configuration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 mkdir -p ~/.streamlit/
-echo "\
+
+echo "[general]\n\
+email = \"mpetrou74@hotmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+cp -f config-template.toml ~/.streamlit/config.toml
+
+echo "\n\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
