eccc617f1281b769585dbbb10b05fda3ab08b282
Michael Petrou <mpetrou74@hotmail.com>
2025-03-25 19:32:15 +1030

    refactor education section: remove markdown file, implement JSON data loading, and enhance display functionality

--- src/education.py
+++ src/education.py
@@ -6,7 +6,29 @@ showcasing academic background and educational achievements.
 """
 
 import streamlit as st
+import json
+import itertools
 from utils import load_markdown
+
+
+def show_section(section):
+
+    for item in section:
+        st.markdown(f"#### {item[1]["title"]}")
+
+        details = f"- {item[1]["line1"]}"
+        if "line2" in item[1]:
+            details = f"{details}\n- {item[1]["line2"]}"
+        if "line3" in item[1]:
+            details = f"{details}\n- {item[1]["line3"]}"
+        if "line4" in item[1]:
+            details = f"{details}\n- {item[1]["line4"]}"
+        if "line5" in item[1]:
+            details = f"{details}\n- {item[1]["line5"]}"
+
+        st.markdown(details)
+
+        st.markdown(f" ")
 
 
 def show_education():
@@ -15,9 +37,25 @@ def show_education():
     
     Renders a header for the education section in the Streamlit app.
     """
-    with st.container(border=True):
-        st.header("Education")
-        st.markdown("---")
+    with open(f"content/education.json", "r") as file:
+        data = json.load(file)
 
-        md = load_markdown("education")
-        st.markdown(md)
+    main_title = data["main_title"]
+    other_title = data["other_title"]
+
+    with st.container(border=True):
+        unused_1, col1, unused_2, col2, unused_3 = st.columns([0.05, 0.4, 0.1, 0.4, 0.05])
+
+        with col1:
+            st.markdown(f"## {main_title}")
+            st.markdown("---")
+
+            main = data["main"]
+            show_section(main.items())
+
+        with col2:
+            st.markdown(f"## {other_title}")
+            st.markdown("---")
+
+            other = data["other"]
+            show_section(other.items())
